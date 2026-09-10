05c9eccc4b8d1d2929c57761686cbe3cb5da2547
werhealthy <122026028+werhealthy@users.noreply.github.com>
2026-09-10 12:36:27 +0200

    feat: load Step 2 hero motion styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/tokens.css'
 import './styles/global.css'
+import './styles/motion.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
