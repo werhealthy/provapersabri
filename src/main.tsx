9c6b9e25666be9bb1a6777f9ca280dd6b05ba1ea
werhealthy <122026028+werhealthy@users.noreply.github.com>
2026-09-10 12:40:18 +0200

    feat: load Step 3 method styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles/tokens.css'
 import './styles/global.css'
 import './styles/motion.css'
+import './styles/method.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
